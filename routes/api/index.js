da43a8ffb3332b78b957ff518d34c7989dd1f495
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-07-14 12:57:26 -0700

    added html routes, added outline for get and post routes for notes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 const notesRoutes = require('./notes');
 
-
-
-
+router.use('/notes', notesRoutes);
 
 
 module.exports = router;
--- routes/api/notes.js
+++ routes/api/notes.js
@@ -2,9 +2,16 @@ const router = require('express').Router();
 const createNew = require('../../helpers/createNew.js');
 const deleteNote = require('../../helpers/deleteNote.js');
 
+router.get('/notes', (req,res)=>{
+    console.log('notes GET success');
+    //read db.json file and return all saved notes as json
+});
 
-
-
+router.post('/notes', (req,res)=>{
+    console.log('notes POST sucess');
+    //receive a new note to save on the req body, add it to the db.json file, and then return the new note to the client
+    //each notes will need a unique id
+})
 
 
 
