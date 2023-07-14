cc44e4bd4e84ca2a66eaaa5a3c351047aaab072b
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-07-14 12:33:00 -0700

    added routes folders and index.js files

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+const api = require('./routes/api');
+const html = require('./routes/html')
+const path = require('path');
+
+//middleware for parsing JSON and urlencoded form data
+app.use('/api', api);
+app.use('/', html);
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static('public'));
+
+app.listen(PORT, ()=>{
+    console.log(`app listening to port ${PORT}`);
+});
