efdd82fd2cafb604039fda0f010c2db9226674a8
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-07-14 13:18:24 -0700

    edited routes, trying to find unexpected token

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const createNew = require('../../helpers/createNew.js');
-const deleteNote = require('../../helpers/deleteNote.js');
+// const createNote = require('../../helpers/createNote.js');
+// const deleteNote = require('../../helpers/deleteNote.js');
+let notesArr = require('../../db/db.json');
 
 router.get('/notes', (req,res)=>{
     console.log('notes GET success');
@@ -11,6 +12,12 @@ router.post('/notes', (req,res)=>{
     console.log('notes POST sucess');
     //receive a new note to save on the req body, add it to the db.json file, and then return the new note to the client
     //each notes will need a unique id
+});
+
+router.delete('/notes/:id', (req,res)=>{
+    console.log('notes DELETE success');
+    //should receive a query parameter that contains the id of a note to delete
+    //will need to read all ntoes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file
 })
 
 
